4caf3d4ca598d78e4798993aacd091e87b3899c6
Mohamed Yasser <134814075+Moyasser05@users.noreply.github.com>
2025-05-11 11:55:06 +0300

    slove

--- src/components/Website/Categories.vue
+++ src/components/Website/Categories.vue
@@ -51,10 +51,10 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 import { nextTick } from 'vue'
-import $ from 'jquery'
-import 'slick-carousel'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+
+// import 'slick-carousel'
+// import 'slick-carousel/slick/slick.css'
+// import 'slick-carousel/slick/slick-theme.css'
 
 export default {
   name: 'WebsiteCategories',
