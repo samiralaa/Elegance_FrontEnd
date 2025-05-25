c16529229ebee79d3644f48b939af966941a4c1a
Samir Alaa <76245712+samiralaa@users.noreply.github.com>
2025-05-25 16:56:31 +0400

    Update Order.vue

--- src/views/Website/Order.vue
+++ src/views/Website/Order.vue
@@ -192,7 +192,7 @@ export default {
   gap: 10px;
 }
 
-.orders-header .order-stage {
+.order-stage {
   text-decoration: none;
   display: flex;
   align-items: center;
@@ -205,7 +205,7 @@ export default {
   transition: all 0.3s ease;
 }
 
-.order-stage .num {
+.num {
   background-color: #fff;
   aspect-ratio: 1 / 1;
   padding: 0.15rem 0.3rem;
@@ -216,15 +216,15 @@ export default {
   font-size: 0.8rem !important;
 }
 
-.orders-header .order-stage:active{
+.order-stage:active{
   background-color: #fff;
 }
-.orders-header .order-stage:hover {
+.order-stage:hover {
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   color: #8b6b3d;
 }
 
-.orders-header .order-stage:active .num {
+.order-stage:active .num {
   color: #fff;
   background-color: #8b6b3d;
 }
